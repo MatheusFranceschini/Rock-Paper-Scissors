d881e938702146d6b81a755bd4ac964bf60fa499
MatheusFranceschini <masuzena@gmail.com>
2025-02-04 17:34:44 -0300

    Upload project to repository

--- Jokempo/GameViewController.swift
+++ Jokempo/GameViewController.swift
@@ -6,3 +6,100 @@
 //
 
 import Foundation
+import UIKit
+
+class GameViewController: UIViewController {
+    @IBOutlet var buttonsCollection: [UIButton]!
+    @IBOutlet weak var escolhaAdversarioLabel: UILabel!
+    @IBOutlet weak var victoryLabel: UILabel!
+    @IBOutlet weak var escolhaJogadorLabel: UILabel!
+    @IBOutlet weak var scoreJogadorLabel: UILabel!
+    @IBOutlet weak var scoreAdversarioLabel: UILabel!
+    
+    var scoreJogador: Int = 0
+    var scoreAdversario: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureButtons()
+    }
+    
+    func configureButtons() {
+        for button in buttonsCollection {
+            button.layer.cornerRadius = 16
+        }
+    }
+    
+    func jogadaAdversario() -> String {
+        let index = Int.random(in: 0...2)
+        let jogadaAdversario: String = ["✊🏻", "✋🏻", "✌🏻"][index]
+        
+        escolhaAdversarioLabel.text = jogadaAdversario
+        
+        return jogadaAdversario
+    }
+    
+    func configureLabel(player: String) {
+        escolhaJogadorLabel.text = player
+    }
+    
+    func verifyWinner(button: UIButton, jogadaAdversario: String) {
+        guard let title = button.titleLabel?.text else { return }
+        
+        if title == "✊🏻" && jogadaAdversario == "✊🏻" {
+            victoryLabel.text = "It's a tie! 😬"
+            configureLabel(player: title)
+        } else if title == "✊🏻" && jogadaAdversario == "✋🏻" {
+            victoryLabel.text = "You lose! ❌"
+            configureLabel(player: title)
+            incrementScoreEnemy()
+        } else if title == "✊🏻" && jogadaAdversario == "✌🏻" {
+            victoryLabel.text = "You won! ⭐️"
+            configureLabel(player: title)
+            incrementScorePlayer()
+        }
+        
+        if title == "✋🏻" && jogadaAdversario == "✊🏻" {
+            victoryLabel.text = "You won! ⭐️"
+            configureLabel(player: title)
+            incrementScorePlayer()
+        } else if title == "✋🏻" && jogadaAdversario == "✋🏻" {
+            victoryLabel.text = "It's a tie! 😬"
+            configureLabel(player: title)
+        } else if title == "✋🏻" && jogadaAdversario == "✌🏻" {
+            victoryLabel.text = "You lose! ❌"
+            configureLabel(player: title)
+            incrementScoreEnemy()
+        }
+        
+        if title == "✌🏻" && jogadaAdversario == "✊🏻" {
+            victoryLabel.text = "You lose! ❌"
+            configureLabel(player: title)
+            incrementScoreEnemy()
+        } else if title == "✌🏻" && jogadaAdversario == "✋🏻" {
+            victoryLabel.text = "You won! ⭐️"
+            configureLabel(player: title)
+            incrementScorePlayer()
+        } else if title == "✌🏻" && jogadaAdversario == "✌🏻" {
+            victoryLabel.text = "It's a tie! 😬"
+            configureLabel(player: title)
+        }
+        
+    }
+    
+    func incrementScorePlayer() {
+        scoreJogador += 1
+        scoreJogadorLabel.text = "Score: \(scoreJogador)"
+    }
+    
+    func incrementScoreEnemy() {
+        scoreAdversario += 1
+        scoreAdversarioLabel.text = "Score: \(scoreAdversario)"
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let jogadaAdversario = jogadaAdversario()
+        verifyWinner(button: sender, jogadaAdversario: jogadaAdversario)
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Rock-Paper-Scissors
+# ✊🏻 Rock Paper Scissors ✋🏻 (pt-BR)
+
+Aplicativo móvel desenvolvido para simular um jogo de Pedra, Papel e Tesoura.
+
+---
+
+## 🛠️ Technologies Used
+O desenvilvimento deste App se utilizou dos seguintes conceitos da linguagem de programação **Swift**:
+
+- **Funções** para melhor estruturação do código
+- **Layout responsivo** utilizando constraints
+- **IBActions** e **IBOutlets**
+
+---
+
+## 📷 App Screenshots
+> *(Add screenshots here to showcase the app's interface and features.)*
+
+---
+
+## 📲 Fique à vontade para experimentar!
+1. Clone este repositório:
+   ```bash
+   git clone https://github.com/your-username/animal-soundboard.git
+   ```
+2. Abra o projeto no **Xcode**.
+3. Conecte um **simulador** ou **dispositivo físico**.
+4. Compile e execute o projeto! 🎉
+
+---
+
+# ✊🏻 Rock Paper Scissors ✋🏻 (en-US)
+
+A mobile app developed to simulate a Rock, Paper, Scissors game.
+
+---
+
+## 🛠️ Technologies Used
+The development of this app utilized the following **Swift** programming concepts:
+
+- **Functions** for better code structure  
+- **Responsive layout** using constraints  
+- **IBActions** and **IBOutlets**  
+
+---
+
+## 📷 App Screenshots
+> *(Add screenshots here to showcase the app's interface and features.)*
+
+---
+
+## 📲 Feel free to try it out!
+1. Clone this repository:
+   ```bash
+   git clone https://github.com/your-username/rock-paper-scissors.git
+   ```
+2. Open the project in **Xcode**.
+3. Connect a simulator or physical device.
+4. Build and run the project! 🎉
+
+#### Developed by Matheus Franceschini!
